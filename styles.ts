a8d73ca2264b4c398855476879f4f621a1bfd8ba
Sam Jones <45644425+jones-sam@users.noreply.github.com>
2022-11-01 15:52:42 -0300

    style file

---
+++ styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchBar: {
+    borderWidth: 1,
+    width: "80%",
+    height: 40,
+    padding: 10,
+    marginTop: 16,
+    borderRadius: 8,
+  },
+});
